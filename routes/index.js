b286eebca465f4b656537a9b384a8006c2657389
Tony Kelly <Tonykelly1286@gmail.com>
2017-05-29 22:07:41 -0500

    Login System Added

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@
 var express = require('express');
 var router = express.Router();
 
-//Get Homepage
-router.get('/', function (req, res) {
+// Get Homepage
+router.get('/', ensureAuthenticated, function(req, res){
     res.render('index');
 });
+
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
 
 module.exports = router;
