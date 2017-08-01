0a8e0b18ee298dc6c0818363668685d8f392dc15
Tony Kelly <Tonykelly1286@gmail.com>
2017-07-31 22:14:18 -0500

    fix: Moved observables into FavListVM View Modle.
    Was able to refactor observables into a view model and get the favlist to render. Still need to get the correct places information to render when clicked.

--- public/js/LocationViewModel.js
+++ public/js/LocationViewModel.js
@@ -1,34 +1,20 @@
-var savedLocations = ko.observableArray();
-  var showPic = ko.observable(false);
- var initPic = function(location){
-     console.log("initPic working " + location.title);
-     if(location.showPic == false){
-         console.log(location.showPic)
+/* var savedLocations = ko.observableArray();
+var showPic = ko.observable(false);
+var picINIT2 = function (location) {
+    console.log("initPic working " + self.location.title);
+    if (self.location.showPic == false) {
+        console.log(self.location.showPic)
         self.showPic(true);
-        location.showPic = true;
+        self.location.showPic = true;
         console.log(location.showPic);
-     }
- };
-var eachLocationCLicked = function (location){
-    console.log(location.title + " was clicked " + location.photo)
-   initPic(location);
-};
-
-var removeLocation = function (location){
-    console.log(location.title + " remove was clicked " + location.photo);
-    savedLocations.remove(location);
-// WRITE AJAX CALL TO UPDATE SAVEDLOACTIONS LIST IN MONGODB
-};
-
- /* var FavListVM = {
-    
-    eachLocationCLicked: function(location){
-        console.log(location.title + " was clicked")
-    showPic(true); 
     }
-}; */
+};
+var eachLocationCLicked = function (location) {
+    console.log(location.title + " was clicked " + location.photo)
+    initPic(location);
+};
 
-function FavListVM(){
+function FavListVM() {
     var self = this;
     self.location = ko.observable(location);
     self.location.title = ko.observable(location.title);
@@ -37,81 +23,117 @@ function FavListVM(){
     self.location.photo = ko.observable(location.photo);
     self.location.favid = ko.observable(location.favid);
     self.location.showPic = ko.observable(location.showPic);
-    
-    
-    self.initPic = function(location){
-        console.log("initPic working " + self.location.title());
-     if(self.location.showPic() == false){
-         console.log(self.location.showPic())
-         showPic(true);
-        self.location.showPic() = true;
-        console.log(self.location.showPic());
-    };
 
-}    
+    self.initPic = function (location) {
+        console.log("initPic working " + self.location.title());
+        if (self.location.showPic() == false) {
+            console.log(self.location.showPic())
+            showPic(true);
+            self
+                .location
+                .showPic() = true;
+            console.log(self.location.showPic());
+        };
+
+    }
 }
+*/
+
+
+
+
+
+var removeLocation = function (location) {
+    console.log(location.title + " remove was clicked " + location.photo);
+    savedLocations.remove(location);
+    // WRITE AJAX CALL TO UPDATE SAVEDLOACTIONS LIST IN MONGODB
+};
+
+var FavListVM = {
+
+    location: ko.observable(location),
+    savedLocations: ko.observableArray(),
+    title: ko.observable(location.title),
+    address: ko.observable(location.address),
+    phone: ko.observable(location.phone),
+    photo: ko.observable(location.photo),
+    favid: ko.observable(location.favid),
+    showPic: ko.observable(location.showPic),
+    
+    initPic: function (location) {
+        console.log("initPic working " + FavListVM.showPic);
+        if (FavListVM.showPic == true) {
+            console.log(FavListVM.showPic)
+            
+            FavListVM.showPic = true;
+            console.log(FavListVM.showPic);
+        } else{
+            console.log("show pic came back true")
+        };
+
+    }
+};
+
 
 function getPlacesInfo(favList) {
 
-        console.log("inside getplacesinfo");
+    console.log("inside getplacesinfo");
 
-        var locphone;
-        var loctitle;
-        var locaddress;
-        var id = 1;
-        var innerHTML2 = document.getElementById('table');
+    var locphone;
+    var loctitle;
+    var locaddress;
+    var id = 1;
+    var innerHTML2 = document.getElementById('table');
 
-        console.log(innerHTML2);
-       var service2= new google
-            .maps
-            .places
-            .PlacesService(innerHTML2);
-        console.log("test:2 fav list has " + favList.length + " items in it");
-        console.log(favList);
-        var favTile = favList[0];
-        for (i = 0; i < favList.length; i++) {
-            var loc = favList[i];
+    console.log(innerHTML2);
+    var service2 = new google
+        .maps
+        .places
+        .PlacesService(innerHTML2);
+    console.log("test:2 fav list has " + favList.length + " items in it");
+    console.log(favList);
+    var favTile = favList[0];
+    for (i = 0; i < favList.length; i++) {
+        var loc = favList[i];
 
-            service2.getDetails({
-                placeId: loc.markerID
-            }, function (place, status) {
-                if (status != google.maps.places.PlacesServiceStatus.OK) {
-                       console.log("no information was passed");
-                   
-                } else {
-                  var location;
-                     if (place.name) {
-                        loctitle = place.name;
-                    }
+        service2.getDetails({
+            placeId: loc.markerID
+        }, function (place, status) {
+            if (status != google.maps.places.PlacesServiceStatus.OK) {
+                console.log("no information was passed");
 
-                    if (place.formatted_address) {
-                        locaddress = place.formatted_address;
-                    }
-                    if (place.formatted_phone_number) {
-                        locphone = place.formatted_phone_number;
-                    }
-                    if (place.photos) {
-                        var photo = place
-                            .photos[0]
-                            .getUrl({maxHeight: 500, maxWidth: 700});
-                    }
-                    location = {
-                        title: loctitle,
-                        address: locaddress,
-                        phone: locphone,
-                        photo: photo,
-                        favid: id,
-                        showPic:false
-                    }
-                    id += 1;
-                  savedLocations.push(location);
-                    console.log("after location is pushed " + savedLocations.length);
-               
-                };  
-            });
-        };
-    
-    
-    }
- ko.applyBindings(new FavListVM(savedLocations.location));   
+            } else {
+                var location;
+                if (place.name) {
+                    loctitle = place.name;
+                }
 
+                if (place.formatted_address) {
+                    locaddress = place.formatted_address;
+                }
+                if (place.formatted_phone_number) {
+                    locphone = place.formatted_phone_number;
+                }
+                if (place.photos) {
+                    var photo = place
+                        .photos[0]
+                        .getUrl({maxHeight: 500, maxWidth: 700});
+                }
+                location = {
+                    title: loctitle,
+                    address: locaddress,
+                    phone: locphone,
+                    photo: photo,
+                    favid: id,
+                    showPic: false
+                }
+                id += 1;
+                FavListVM.savedLocations.push(location);
+                
+
+            };
+        });
+    };
+
+}
+ko.applyBindings(FavListVM );
